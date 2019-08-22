16a957f95713f32c7fecbe9428e7d1f5f9dfacdf
Andy <kxie0003@student.monash.edu>
2019-08-22 20:52:24 +1000

    Initial commit

---
+++ app/src/main/java/com/example/rural_healthy_mom_to_be/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.rural_healthy_mom_to_be;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
